5741b9b990dcd62c2c916affe5f6d64e4e3b05a2
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 10:12:09 +0300

    Update config.py

---
+++ bot/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "xxxx")
+    DB_HOST = os.getenv("DB_HOST", "xxxx")
+    DB_NAME = os.getenv("DB_NAME", "xxxx")
+    DB_USER = os.getenv("DB_USER", "xxxx")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "xxxx")
+    MIN_AGE = 18
+    MAX_VERIFICATION_ATTEMPTS = 3
+    BLOCK_MINUTES = 60
+    SPOILER_CATEGORIES = [1, 8, 9]
+    ADMIN_IDS = [xxxxxxxxx]
+config = Config()
