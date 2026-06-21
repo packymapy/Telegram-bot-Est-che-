f0d94a9f400bfdadcb74fae08e9dfaf84172b18f
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 13:01:48 +0300

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,3 +2,4 @@ from .start import router as start_router
 from .catalog import router as catalog_router
 from .product import router as product_router
 from .search import router as search_router
+from .contacts import router as contacts_router
