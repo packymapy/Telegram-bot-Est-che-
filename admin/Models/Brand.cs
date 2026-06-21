806c2a61edc377c76f6206877e84217dba50b2b7
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 14:35:14 +0300

    Update Brand.cs

--- admin/Models/Brand.cs
+++ admin/Models/Brand.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace AdminApp.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int ProductCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
