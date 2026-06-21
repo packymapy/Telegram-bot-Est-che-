c5ea526767360e1f136d671a203c870dc1836797
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 13:37:16 +0300

    Update Product.cs

--- admin/Models/Product.cs
+++ admin/Models/Product.cs
@@ -1,1 +1,21 @@
+using System;
 
+namespace AdminApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int? BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Details { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
