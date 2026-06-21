4d167497a99622c4e6b07c3c17fda56ca0360844
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 14:35:56 +0300

    Update ModernButton.cs

--- admin/Controls/ModernButton.cs
+++ admin/Controls/ModernButton.cs
@@ -1,1 +1,64 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
 
+namespace AdminApp.Controls
+{
+    public class ModernButton : Button
+    {
+        private Color _hoverColor = Colors.PrimaryDark;
+        private Color _pressColor = Colors.Primary;
+
+        public ModernButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Colors.Primary;
+            this.ForeColor = Color.White;
+            this.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            this.Height = 35;
+            this.Cursor = Cursors.Hand;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (var path = new GraphicsPath())
+            {
+                int radius = 5;
+                path.AddArc(0, 0, radius, radius, 180, 90);
+                path.AddArc(Width - radius, 0, radius, radius, 270, 90);
+                path.AddArc(Width - radius, Height - radius, radius, radius, 0, 90);
+                path.AddArc(0, Height - radius, radius, radius, 90, 90);
+                path.CloseAllFigures();
+
+                this.Region = new Region(path);
+            }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            this.BackColor = _hoverColor;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            this.BackColor = Colors.Primary;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs mevent)
+        {
+            base.OnMouseDown(mevent);
+            this.BackColor = _pressColor;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs mevent)
+        {
+            base.OnMouseUp(mevent);
+            this.BackColor = Colors.Primary;
+        }
+    }
+}
