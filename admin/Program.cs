54c25def748de7962d1567f3a340ad511861685b
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 13:37:04 +0300

    Update Program.cs

--- admin/Program.cs
+++ admin/Program.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Windows.Forms;
+using AdminApp.Forms;
 
+namespace AdminApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var loginForm = new LoginForm())
+            {
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
