8df36c0fa1b2c95ab072cda8eeeac7fa3c6cb4bd
packymapy <115074887+packymapy@users.noreply.github.com>
2026-06-21 14:35:01 +0300

    Update Category.cs

--- admin/Models/Category.cs
+++ admin/Models/Category.cs
@@ -1,1 +1,13 @@
+using System;
 
+namespace AdminApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SortOrder { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
